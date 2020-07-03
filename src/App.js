6b7da9c6c2e3922c0cdd7f84d7be77ee4bc680d9
Zainab imtiaz <zainabimtiaz@Zainabs-MacBook-Pro.local>
2020-07-03 11:16:13 +0530

    added basic codes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import List from './components/List.js';
+import WithLoading from './components/withLoading.js';
+const ListWithLoading = WithLoading(List);
+class App extends React.Component {
+  state = {
+loading:false,
+  };
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch(`https://api.github.com/users/hacktivist123/repos`)
+      .then((json) => json.json())
+      .then((repos) => {
+        this.setState({ loading: false, repos: repos });
+      });
+  }
+  render() {
+     return (
+      <ListWithLoading
+        isLoading={this.state.loading}
+        repos={this.state.repos}
+      />
+    // <List repos={this.state.repos}/>       // This will fetch entire data without loading
+    );
+    
+  }
 }
-
 export default App;
